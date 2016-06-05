44bd5ccc06c1c02c3f94b742252c6f0df6e07ab3
Tim Detwiler <timdetwiler@gmail.com>
2016-06-04 18:48:39 -0700

    Remove hard-coded classfile from javap.

--- src/bin/javap.rs
+++ src/bin/javap.rs
@@ -4,6 +4,7 @@ extern crate log;
 extern crate env_logger;
 
 use std::fs::File;
+use std::env;
 
 use frappe::classfile::reader::ClassReader;
 use frappe::javap::{Disassemble, Formatter, Options};
@@ -11,9 +12,7 @@ use frappe::javap::{Disassemble, Formatter, Options};
 fn main() {
     env_logger::init().unwrap();
     let verbose = true;
-    let classname = "io.hcf.frappe.HelloWorld";
-    let mut class_filename = classname.replace('.', "/");
-    class_filename.push_str(".class");
+    let class_filename = env::args().nth(1).expect("usage: class_reader <class file>");
     let class_file = File::open(class_filename).unwrap();
     let mut fmt = Formatter::new();
     let class = ClassReader::new(class_file).read_class().unwrap();
